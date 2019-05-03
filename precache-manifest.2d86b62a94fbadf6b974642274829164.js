7c3f55d36d32a675fc5c874ff350c64d80f5c963
Christian Valentin <chris_valentin@outlook.com>
2019-05-03 14:45:17 -0300

    Updates

---
+++ precache-manifest.2d86b62a94fbadf6b974642274829164.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0edd79606871ee4dd9e8",
+    "url": "/blz-checkout/static/js/4.d6c2316c.chunk.js"
+  },
+  {
+    "revision": "c7f45b3e452ad62e68ea",
+    "url": "/blz-checkout/static/css/main.23c2c55d.chunk.css"
+  },
+  {
+    "revision": "26f2715e4207a027143d",
+    "url": "/blz-checkout/static/js/runtime~main.77daad71.js"
+  },
+  {
+    "revision": "18ed240f0af955e04f2d",
+    "url": "/blz-checkout/static/js/2.d4ee9e03.chunk.js"
+  },
+  {
+    "revision": "3dc60e20b7577545ff81",
+    "url": "/blz-checkout/static/js/3.0bce0326.chunk.js"
+  },
+  {
+    "revision": "c7f45b3e452ad62e68ea",
+    "url": "/blz-checkout/static/js/main.c77cfb22.chunk.js"
+  },
+  {
+    "revision": "c121a6f75d2f8096ed15",
+    "url": "/blz-checkout/static/js/5.9d8aff88.chunk.js"
+  },
+  {
+    "revision": "b167529c5de6b3cb2789",
+    "url": "/blz-checkout/static/js/6.46824953.chunk.js"
+  },
+  {
+    "revision": "802e14683052a34311af",
+    "url": "/blz-checkout/static/js/7.f5818b9c.chunk.js"
+  },
+  {
+    "revision": "d8ea33b39a77d4a636476cb8482046d3",
+    "url": "/blz-checkout/static/media/success-icon.d8ea33b3.png"
+  },
+  {
+    "revision": "41d59540ce3b86f1b1a67b580c368af1",
+    "url": "/blz-checkout/index.html"
+  }
+];
